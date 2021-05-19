24b33f1d3da7a14c2be4579a780edda21e6e71e1
AnnaHelf <anna.helfferich@gmx.net>
2021-05-19 16:47:20 +0200

    19.05.2021

--- js/work.js
+++ js/work.js
@@ -1,9 +1,9 @@
-$(function () {
+/* $(function () {
   $("h6").click(function () {
     console.log("click");
     $(this).parent().siblings().slideToggle();
   });
-});
+}); */
 
 const closeWork = document.querySelector("#close_work");
 const divWork = document.querySelector("#myWork");
